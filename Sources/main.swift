37690182ac68fe15c6978b53681c8f581bce03a1
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2016-03-30 21:17:14 +0200

    merge

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,8 @@
 let window = Window(title:"Test",width:200,height:200)
 let texture = try! Texture(path:"coucou.png")
+let music = try! Music(path:"~/Téléchargements/0933.ogg")
+music.play()
+
 while window.isOpen(){
   window.keyPressed(){key in
     if key.isClosed{
